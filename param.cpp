4b41b5123015e91b5ee326b13ffc51415f3010c1
ethantran03 <123023012+ethantran03@users.noreply.github.com>
2024-09-12 22:05:57 -0500

    Update param.cpp

--- param.cpp
+++ param.cpp
@@ -1,48 +1,28 @@
+// param.cpp
 #include "param.hpp"
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
 
-Param::Param() : inputRedirect(nullptr), outputRedirect(nullptr), background(0), argumentCount(0) {
-    for (int i = 0; i < MAXARGS; ++i) {
-        argumentVector[i] = nullptr;
-    }
+Param::Param() : inputRedirect(NULL), outputRedirect(NULL), background(0), argumentCount(0) {
+    memset(argumentVector, 0, sizeof(argumentVector));
 }
 
 Param::~Param() {
-    delete[] inputRedirect;
-    delete[] outputRedirect;
+    if (inputRedirect) free(inputRedirect);
+    if (outputRedirect) free(outputRedirect);
     for (int i = 0; i < argumentCount; ++i) {
-        delete[] argumentVector[i];
-    }
-}
-
-void Param::setInputRedirect(const char *filename) {
-    delete[] inputRedirect;
-    inputRedirect = strdup(filename);
-}
-
-void Param::setOutputRedirect(const char *filename) {
-    delete[] outputRedirect;
-    outputRedirect = strdup(filename);
-}
-
-void Param::setBackground(int flag) {
-    background = flag;
-}
-
-void Param::addArgument(const char *arg) {
-    if (argumentCount < MAXARGS) {
-        argumentVector[argumentCount++] = strdup(arg);
+        free(argumentVector[i]);
     }
 }
 
 void Param::printParams() const {
     std::cout << "Input Redirect: " << (inputRedirect ? inputRedirect : "None") << std::endl;
     std::cout << "Output Redirect: " << (outputRedirect ? outputRedirect : "None") << std::endl;
-    std::cout << "Background: " << (background ? "Yes" : "No") << std::endl;
-    std::cout << "Arguments:" << std::endl;
+    std::cout << "Background: " << background << std::endl;
+    std::cout << "Arguments: ";
     for (int i = 0; i < argumentCount; ++i) {
-        std::cout << "  " << argumentVector[i] << std::endl;
+        std::cout << argumentVector[i] << " ";
     }
+    std::cout << std::endl;
 }
